8eb4ee09890a84aa84e9cc4417c9c95fbf6817c1
Dafinci01 <odelanadavid@gmail.com>
2024-09-15 18:19:50 +0100

    update on social

---
+++ app/home.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from app.forms import LoginForm, RegistrationForm
+
+app = Flask(__name__)
+# Configure a secret key for the Flask application to prevent CSRF (Cross-Site Request Forgery) attacks
+app.config['SECRET_KEY'] = '6a031eb93693472f4d44bb82ddb5dad8'
+
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'David Odelana',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Handle login logic here
+        flash(f'Login requested for {form.email.data}!', 'info')
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
